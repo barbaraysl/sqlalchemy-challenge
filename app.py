063551a7880d8807e43d1402fe2a38981be0c073
Shenglin Ye <barbaraysl@gmail.com>
2019-11-24 16:01:57 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,1 +1,106 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all precipitation names"""
+    # Query all precipitation
+    results = session.query(Measurement.prcp,Measurement.date).all()
+
+    session.close()
+    all_prcp = []
+    for prcp,date in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_prcp.append(prcp_dict)
+
+    return jsonify(all_prcp)
+
+@app.route("/api/v1.0/stations")
+def station():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all station names"""
+    # Query all stations
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_station = list(np.ravel(results))
+
+    return jsonify(all_station)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of temperature over last year"""
+    # Query all stations
+    results = session.query(Measurement.station,Measurement.date,Measurement.tobs).filter(Measurement.station=="USC00519281").filter( Measurement.date >'2016-08-23').all()
+    
+    session.close()
+
+    all_tobs = []
+    for station,date,tobs in results:
+        temp_dict = {}
+        temp_dict["station"] = station
+        temp_dict["date"] = date
+        temp_dict["tobs"] = tobs
+        all_tobs.append(temp_dict)
+
+    return jsonify(all_tobs)
+
+  
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
